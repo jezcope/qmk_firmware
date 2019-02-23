9cbe2a3c25f02420c8b75b24f621b2deac2faa69
Jez Cope <j.cope@erambler.co.uk>
2019-02-23 10:11:45 +0000

    Modifier tweaks

--- keyboards/laplace/keymaps/petrichor/keymap.c
+++ keyboards/laplace/keymaps/petrichor/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = LAYOUT(
     KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,  KC_DEL,
-    KC_LCTL,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_ENT,
-    KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,     KC_RSFT,
-    KC_ESC,   KC_LALT,  KC_LGUI,  GU_RAISE,      BS_RAISE,           SP_LOWER,      KC_RGUI,  KC_RALT,  LOWER,    KC_RCTL
+    KC_LCTL,    KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,             KC_ENT,
+    KC_LSFT,        KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,         KC_RSFT,
+    KC_ESC,   GU_RAISE, KC_LGUI,  KC_LALT,       BS_RAISE,           SP_LOWER,        KC_RALT,  KC_RGUI,  LOWER,            KC_RCTL
   ),
 
   [_QWERTY] = LAYOUT(
