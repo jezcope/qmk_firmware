a860f289ef3c56c91fc0ce7edefb68cf34d2c5e5
Jez Cope <j.cope@erambler.co.uk>
2019-02-24 21:39:26 +0000

    Add Space Cadet Shift

--- keyboards/laplace/keymaps/petrichor/keymap.c
+++ keyboards/laplace/keymaps/petrichor/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK] = LAYOUT(
     KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,  KC_DEL,
     KC_LCTL,    KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,             KC_ENT,
-    KC_LSFT,         KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,        KC_RSFT,
+    KC_LSPO,         KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,        KC_RSPC,
     KC_ESC,   GU_RAISE, KC_LGUI,  KC_LALT,       BS_RAISE,           SP_LOWER,        KC_RALT,  KC_RGUI,       LOWER,       KC_RCTL
   ),
 
   [_QWERTY] = LAYOUT(
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_DEL,   KC_BSPC,
     KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_QUOT,          KC_ENT,
-    KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,     KC_RSFT,
+    KC_LSPO,         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,     KC_RSPC,
     KC_LCTL,  KC_LALT,  KC_LGUI,  GU_RAISE,      BS_RAISE,           SP_LOWER,        KC_RGUI,  KC_RALT,       LOWER,       KC_RCTL
   ),
 
