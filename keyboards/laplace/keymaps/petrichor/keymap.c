815621cb2c610a7ecbd445560cd874320c819393
Jez Cope <j.cope@erambler.co.uk>
2019-03-04 19:34:19 +0000

    Add a lock key

--- keyboards/laplace/keymaps/petrichor/keymap.c
+++ keyboards/laplace/keymaps/petrichor/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,  KC_DEL,
     KC_LCTL,    KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,             KC_ENT,
     KC_LSPO,         KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,        KC_RSPC,
-    KC_ESC,   T_MOUSE,  KC_LGUI,  KC_LALT,       BS_FN1,             SP_FN2,          KC_RALT,  KC_RGUI,       GU_FN1,      KC_RCTL
+    KC_ESC,   T_MOUSE,  KC_LGUI,  KC_LALT,       BS_FN1,             SP_FN2,          KC_RALT,  KC_LOCK,       GU_FN1,      KC_RCTL
   ),
 
   [_QWERTY] = LAYOUT(
