745d11cd4e75989a7faccbf672a9b441adc9162f
Jez Cope <j.cope@erambler.co.uk>
2019-04-09 21:51:12 +0100

    Remove QWERTY layer for now

--- keyboards/keebio/laplace/keymaps/petrichor/keymap.c
+++ keyboards/keebio/laplace/keymaps/petrichor/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-enum layers {_DVORAK, _QWERTY, _FN1, _FN2, _MOUSE};
+enum layers {_DVORAK, /* _QWERTY, */ _FN1, _FN2, _MOUSE};
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -10,7 +10,7 @@ enum layers {_DVORAK, _QWERTY, _FN1, _FN2, _MOUSE};
 #define MOUSE MO(_MOUSE)
 #define T_MOUSE TT(_MOUSE)
 #define DF_DVOR DF(_DVORAK)
-#define DF_QWER DF(_QWERTY)
+/* #define DF_QWER DF(_QWERTY) */
 #define SP_FN1 LT(_FN1, KC_SPACE)
 #define SP_FN2 LT(_FN2, KC_SPACE)
 #define BS_FN1 LT(_FN1, KC_BSPC)
@@ -30,13 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   T_MOUSE,  KC_LGUI,  KC_LALT,       BS_FN1,             SP_FN2,          KC_RALT,  KC_LOCK,       GU_FN1,      KC_RCTL
   ),
 
-  [_QWERTY] = LAYOUT(
-    KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_DEL,   KC_BSPC,
-    KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_QUOT,          KC_ENT,
-    KC_LSPO,         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,     KC_RSPC,
-    KC_LCTL,  T_MOUSE,  KC_LGUI,  GU_FN1,        BS_FN1,             SP_FN2,          KC_RGUI,  KC_RALT,       GU_FN1,      KC_RCTL
-  ),
-
   [_FN1] = LAYOUT(
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,
     _______,    _______,  _______,  _______,  KC_VOLU,  KC_LBRC,  KC_RBRC,  KC_4,     KC_5,     KC_6,     KC_SCLN,          KC_BSLASH,
@@ -46,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FN2] = LAYOUT(
     KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,  KC_PLUS,
-    _______,    DF_DVOR,  KC_WBAK,  CT_PGUP,  KC_PGUP,  KC_HOME,  _______,  _______,  _______,  _______,  KC_COLN,          KC_PIPE,
-    _______,         DF_QWER,  KC_WFWD,  CT_PGDN,  KC_PGDN,  KC_END,   _______,  _______,  _______,  _______,  _______,     _______,
+    _______,    _______,  KC_WBAK,  CT_PGUP,  KC_PGUP,  KC_HOME,  _______,  _______,  _______,  _______,  KC_COLN,          KC_PIPE,
+    _______,         _______,  KC_WFWD,  CT_PGDN,  KC_PGDN,  KC_END,   _______,  _______,  _______,  _______,  _______,     _______,
     _______,  _______,  _______,  _______,       KC_DEL,             _______,         _______,  _______,       _______,     RESET
   ),
 
@@ -56,5 +49,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    _______,  _______,  _______,  _______,  _______,  KC_WH_U,  KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,          _______,
     _______,         _______,  _______,  _______,  _______,  _______,  KC_WH_D,  _______,  _______,  _______,  _______,     _______,
     _______,  _______,  _______,  _______,       KC_BTN2,            KC_BTN1,         _______,  _______,       _______,     _______
-  )
+  ),
+
+  /* [_QWERTY] = LAYOUT( */
+  /*   KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_DEL,   KC_BSPC, */
+  /*   KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_QUOT,          KC_ENT, */
+  /*   KC_LSPO,         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,     KC_RSPC, */
+  /*   KC_LCTL,  T_MOUSE,  KC_LGUI,  GU_FN1,        BS_FN1,             SP_FN2,          KC_RGUI,  KC_RALT,       GU_FN1,      KC_RCTL */
+  /* ), */
+
+  /* [_TEMPLATE] = LAYOUT( */
+  /*   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, */
+  /*   _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,          _______, */
+  /*   _______,         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______, */
+  /*   _______,  _______,  _______,  _______,       _______,            _______,         _______,  _______,       _______,     _______ */
+  /* ), */
 };
