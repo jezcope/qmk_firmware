138f911ec5fddbd6807cbb828b36c728c86f8639
Jez Cope <j.cope@erambler.co.uk>
2019-04-09 21:58:29 +0100

    Initial attempt at a macro layer

--- keyboards/keebio/laplace/keymaps/petrichor/keymap.c
+++ keyboards/keebio/laplace/keymaps/petrichor/keymap.c
@@ -1,12 +1,17 @@
 #include QMK_KEYBOARD_H
 
-enum layers {_DVORAK, /* _QWERTY, */ _FN1, _FN2, _MOUSE};
+enum layers {_DVORAK, _FN1, _FN2, _MACRO, _MOUSE};
+
+enum custom_keycodes {
+  M_BEST = SAFE_RANGE,
+};
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define FN1 MO(_FN1)
-#define FN2 MO(_FN2)
+#define KC_FN1 MO(_FN1)
+#define KC_FN2 MO(_FN2)
+#define KC_MACR MO(_MACRO)
 #define MOUSE MO(_MOUSE)
 #define T_MOUSE TT(_MOUSE)
 #define DF_DVOR DF(_DVORAK)
@@ -27,12 +32,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,  KC_DEL,
     KC_LCTL,    KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,             KC_ENT,
     KC_LSPO,         KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,        KC_RSPC,
-    KC_ESC,   T_MOUSE,  KC_LGUI,  KC_LALT,       BS_FN1,             SP_FN2,          KC_RALT,  KC_LOCK,       GU_FN1,      KC_RCTL
+    KC_ESC,   T_MOUSE,  KC_LGUI,  KC_LALT,       BS_FN1,             SP_FN2,          KC_RALT,  KC_MACR,       GU_FN1,      KC_RCTL
   ),
 
   [_FN1] = LAYOUT(
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,
-    _______,    _______,  _______,  _______,  KC_VOLU,  KC_LBRC,  KC_RBRC,  KC_4,     KC_5,     KC_6,     KC_SCLN,          KC_BSLASH,
+    _______,    M_BEST,   _______,  _______,  KC_VOLU,  KC_LBRC,  KC_RBRC,  KC_4,     KC_5,     KC_6,     KC_SCLN,          KC_BSLASH,
     _______,         _______,  _______,  _______,  KC_VOLD,  KC_LCBR,  KC_RCBR,  KC_1,     KC_2,     KC_3,     KC_UP,       _______,
     _______,  _______,  _______,  _______,       _______,            KC_DEL,          KC_0,     KC_LEFT,       KC_DOWN,     KC_RGHT
   ),
@@ -51,6 +56,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,  _______,       KC_BTN2,            KC_BTN1,         _______,  _______,       _______,     _______
   ),
 
+  [_MACRO] = LAYOUT(
+    _______,  M_BEST,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,          _______,
+    _______,         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,
+    _______,  _______,  _______,  _______,       _______,            _______,         _______,  _______,       _______,     _______
+  ),
+
   /* [_QWERTY] = LAYOUT( */
   /*   KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_DEL,   KC_BSPC, */
   /*   KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_QUOT,          KC_ENT, */
@@ -64,4 +76,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*   _______,         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______, */
   /*   _______,  _______,  _______,  _______,       _______,            _______,         _______,  _______,       _______,     _______ */
   /* ), */
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case M_BEST:
+    if (record->event.pressed) {
+      SEND_STRING("Best wishes,\nJez");
+    }
+    break;
+
+  }
+  return true;
 };
