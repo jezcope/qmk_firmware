9dc198695e1c09c4b4ce6f14723999a4066bf6fa
Jez Cope <j.cope@erambler.co.uk>
2019-05-05 12:11:01 +0100

    Implement Super ALT↯TAB macro

--- users/petrichor/petrichor.c
+++ users/petrichor/petrichor.c
@@ -2,6 +2,9 @@
 #include "petrichor.h"
 #include "rot13.h"
 
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
 char s_email[22] = "w.pbcr@renzoyre.pb.hx";
 char s_email_work[15] = "wrm.pbcr@oy.hx";
 
@@ -22,6 +25,15 @@ void keyboard_post_init_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     switch (keycode) {
+    case ALT_TAB:
+	  if (!is_alt_tab_active) {
+		is_alt_tab_active = true;
+		register_code(KC_LALT);
+	  }
+	  alt_tab_timer = timer_read();
+	  register_code(KC_TAB);
+      break;
+
     case M_BEST:
       SEND_STRING("Best wishes,\nJez"); break;
     case M_RGRDS:
@@ -35,6 +47,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case M_EML_W:
       send_string(s_email_work); break;
     }
+  } else {
+	switch (keycode) {
+	case ALT_TAB:
+	  unregister_code(KC_TAB);
+	  break;
+	}
   }
+
   return true;
 };
+
+void matrix_scan_user(void) {
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LALT);
+      is_alt_tab_active = false;
+    }
+  }
+}
--- users/petrichor/petrichor.h
+++ users/petrichor/petrichor.h
@@ -4,6 +4,7 @@ enum custom_keycodes {
   M_BEST = SAFE_RANGE,
   M_RGRDS, M_THNKS, M_CHRS,
   M_EML_H, M_EML_W,
+  ALT_TAB,
   NEW_SAFE_RANGE
 };
 
