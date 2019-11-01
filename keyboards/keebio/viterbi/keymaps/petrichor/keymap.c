920b228c360d239c97d43def28863ae6d44a211c
Jez Cope <j.cope@erambler.co.uk>
2019-11-01 21:44:52 +0000

    Consistent arrows and separate numpad layer for Viterbi

--- keyboards/keebio/viterbi/keymaps/petrichor/keymap.c
+++ keyboards/keebio/viterbi/keymaps/petrichor/keymap.c
@@ -3,7 +3,7 @@
 
 extern keymap_config_t keymap_config;
 
-enum layers { _DVORAK, _QWERTY, _FN1, _FN2, _FN3, _MOUSE };
+enum layers { _DVORAK, _QWERTY, _FN1, _FN2, _FN3, _FN4, _MOUSE };
 
 #define KC_FN1 MO(_FN1)
 #define KC_FN2 MO(_FN2)
@@ -12,6 +12,7 @@ enum layers { _DVORAK, _QWERTY, _FN1, _FN2, _FN3, _MOUSE };
 #define SP_FN2 LT(_FN2, KC_SPACE)
 #define MOUSE MO(_MOUSE)
 #define T_MOUSE TT(_MOUSE)
+#define T_FN4 TT(_FN4)
 #define DF_DVOR DF(_DVORAK)
 #define DF_QWER DF(_QWERTY)
 
@@ -22,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_WBAK, KC_TAB , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   ,     KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_SLSH, KC_EQL,
     KC_WFWD, KC_LCTL, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   ,     KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_MINS, KC_ENT ,
     KC_PGUP, KC_LSPO, KC_SCLN, KC_Q   , KC_J   , KC_K   , KC_X   ,     KC_B   , KC_M   , KC_W   , KC_V   , KC_Z   , KC_RSPC, KC_BSLS,
-    KC_PGDN, KC_ESC , T_MOUSE, KC_LGUI, KC_LALT, KC_FN3 , BS_FN1 ,     SP_FN2 , KC_RALT, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_RCTL
+    KC_PGDN, KC_ESC , T_MOUSE, KC_LGUI, KC_LALT, KC_FN3 , BS_FN1 ,     SP_FN2 , KC_RALT, _______, _______, _______, T_FN4  , KC_RCTL
   ),
 
   [_QWERTY] = LAYOUT_ortho_5x14(
@@ -34,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN1] = LAYOUT_ortho_5x14(
-	KC_INS , KC_TILD, KC_F1  , KC_F2  , KC_F3  , ALT_F4 , KC_F5  ,     _______, _______, KC_PSLS, KC_PAST, KC_PMNS, KC_NLCK, _______,
-    KC_TBAK, _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     _______, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_LCBR, KC_RCBR,
-    KC_TFWD, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     _______, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_LBRC, KC_RBRC,
-    KC_HOME, _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,     _______, KC_P1  , KC_P2  , KC_P3  , KC_PENT, KC_LPRN, KC_RPRN,
-    KC_END , _______, _______, _______, _______, _______, _______,     _______, KC_P0  , KC_P0  , KC_PDOT, KC_PENT, _______, _______
+	KC_INS , KC_TILD, KC_F1  , KC_F2  , KC_F3  , ALT_F4 , KC_F5  ,     _______, _______, _______, _______, _______, _______, _______,
+    KC_TBAK, _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     _______, KC_WBAK, KC_WFWD, KC_TBAK, KC_TFWD, KC_LCBR, KC_RCBR,
+    KC_TFWD, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     KC_PGUP, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_LBRC, KC_RBRC,
+    KC_HOME, _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,     KC_PGDN, KC_HOME, KC_END , _______, _______, KC_LPRN, KC_RPRN,
+    KC_END , _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_FN2] = LAYOUT_ortho_5x14(
@@ -55,6 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     UC_M_WI, DF_QWER, _______, _______, _______, _______, _______,     _______, CUT_ALL, CPY_ALL, _______, _______, _______, _______,
     UC_M_WC, DF_DVOR, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
     UC_M_LN, RESET  , _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______
+  ),
+
+  [_FN4] = LAYOUT_ortho_5x14(
+    KC_INS , KC_TILD, KC_F1  , KC_F2  , KC_F3  , ALT_F4 , KC_F5  ,     _______, _______, KC_PSLS, KC_PAST, KC_PMNS, KC_NLCK, _______,
+    KC_TBAK, _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     _______, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_LCBR, KC_RCBR,
+    KC_TFWD, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     _______, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_LBRC, KC_RBRC,
+    KC_HOME, _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,     _______, KC_P1  , KC_P2  , KC_P3  , KC_PENT, KC_LPRN, KC_RPRN,
+    KC_END , _______, _______, _______, _______, _______, _______,     _______, KC_P0  , KC_P0  , KC_PDOT, KC_PENT, _______, _______
   ),
 
   [_MOUSE] = LAYOUT_ortho_5x14(
