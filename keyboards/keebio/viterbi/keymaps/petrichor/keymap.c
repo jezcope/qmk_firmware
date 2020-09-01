18964787d57262b73bb73061674ca3bb8790930c
Jez Cope <j.cope@erambler.co.uk>
2020-09-01 13:29:07 +0100

    Viterbi: Move numlock key to its customary place on the numpad

--- keyboards/keebio/viterbi/keymaps/petrichor/keymap.c
+++ keyboards/keebio/viterbi/keymaps/petrichor/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN4] = LAYOUT_ortho_5x14(
-    KC_INS , KC_TILD, KC_F1  , KC_F2  , KC_F3  , ALT_F4 , KC_F5  ,     _______, _______, KC_PSLS, KC_PAST, KC_PMNS, KC_NLCK, _______,
+    KC_INS , KC_TILD, KC_F1  , KC_F2  , KC_F3  , ALT_F4 , KC_F5  ,     _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______, _______,
     KC_TBAK, _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     _______, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_LCBR, KC_RCBR,
     KC_TFWD, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     _______, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_LBRC, KC_RBRC,
     KC_HOME, _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,     _______, KC_P1  , KC_P2  , KC_P3  , KC_PENT, KC_LPRN, KC_RPRN,
